7107cd60b0b53ba37dd7cde2cbf91549fe4e9bb1
Walker-PI <1443045903@qq.com>
2021-03-29 18:46:10 +0800

    test branch
    Change-Id: Ia0d708e212c6ab044f968aa4f15eb6a513f28c6f

--- router.go
+++ router.go
@@ -10,12 +10,22 @@ import (
 
 func registerRouter(r *gin.Engine) {
 
+	logs.Info("[Test] name=%v", "Bob")
+	logs.Debug("[Test] name=%v", "Tim")
+
 	r.GET("/ping", func(c *gin.Context) {
-		logs.Info("[Test] name=%v", "Bob")
-		logs.Debug("[Test] name=%v", "Tim")
 		c.JSON(http.StatusOK, gin.H{
 			"msg":  "pong",
 			"time": time.Now().Format(time.RFC3339),
+		})
+	})
+
+	r.GET("/test", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"code":    0,
+			"status":  "",
+			"message": "",
+			"data":    []int64{1, 2, 3, 4, 5, 6},
 		})
 	})
 
